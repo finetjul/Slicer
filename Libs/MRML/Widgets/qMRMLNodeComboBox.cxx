finetjul/Slicer

commit 216e9ccd7dba5861add626cd1930aaea3b097233

ENH: Harmonize qMRMLNodeComboBox properties
Previously, the property currentNodeId of the node combobox was set with an
id (QString) and sent a signal with a node (vtkMRMLNode*) which can be
confusing in the widgets using it. Now, the node combobox as an id
property or a node property.

See Issue #1694

git-svn-id: http://svn.slicer.org/Slicer4/trunk@22052 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLNodeComboBox.cxx
@@ -459,7 +459,7 @@ vtkMRMLNode* qMRMLNodeComboBox::currentNode()const
 }
 
 // --------------------------------------------------------------------------
-QString qMRMLNodeComboBox::currentNodeId()const
+QString qMRMLNodeComboBox::currentNodeID()const
 {
   vtkMRMLNode* node = this->currentNode();
   return node ? node->GetID() : "";
@@ -507,6 +507,7 @@ void qMRMLNodeComboBox::emitCurrentNodeChanged()
     {
     emit currentNodeChanged(node);
     emit currentNodeChanged(node != 0);
+    emit currentNodeIDChanged(node ? node->GetID() : "");
     }
 }
 
@@ -593,14 +594,14 @@ void qMRMLNodeComboBox::setMRMLScene(vtkMRMLScene* scene)
   // vtkMRMLClipModels selector in the Models module)
   if (oldNodeCount)
     {
-    this->setCurrentNode(oldCurrentNode);
+    this->setCurrentNodeID(oldCurrentNode);
     }
   // if the new nodeCount is 0, then let's make sure to select 'invalid' node
   // (None(0) or -1). we can't do nothing otherwise the Scene index (rootmodelIndex)
   // would be selected and "Scene" would be displayed (see vtkMRMLNodeComboboxTest5)
   else
     {
-    this->setCurrentNode(this->currentNode());
+    this->setCurrentNodeID(this->currentNodeID());
     }
 
   this->setEnabled(scene != 0);
@@ -609,11 +610,11 @@ void qMRMLNodeComboBox::setMRMLScene(vtkMRMLScene* scene)
 // --------------------------------------------------------------------------
 void qMRMLNodeComboBox::setCurrentNode(vtkMRMLNode* newCurrentNode)
 {
-  this->setCurrentNode(newCurrentNode ? newCurrentNode->GetID() : "");
+  this->setCurrentNodeID(newCurrentNode ? newCurrentNode->GetID() : "");
 }
 
 // --------------------------------------------------------------------------
-void qMRMLNodeComboBox::setCurrentNode(const QString& nodeID)
+void qMRMLNodeComboBox::setCurrentNodeID(const QString& nodeID)
 {
   Q_D(qMRMLNodeComboBox);
   // A straight forward implementation of setCurrentNode would be:

Libs/MRML/Widgets/qMRMLNodeComboBox.h
@@ -56,7 +56,7 @@ class QMRML_WIDGETS_EXPORT qMRMLNodeComboBox
   : public QWidget
 {
   Q_OBJECT
-  Q_PROPERTY(QString currentNodeId READ currentNodeId WRITE setCurrentNode DESIGNABLE false)
+  Q_PROPERTY(QString currentNodeID READ currentNodeID WRITE setCurrentNodeID NOTIFY currentNodeIDChanged DESIGNABLE false)
   Q_PROPERTY(QStringList nodeTypes READ nodeTypes WRITE setNodeTypes)
   Q_PROPERTY(bool showHidden READ showHidden WRITE setShowHidden)
   Q_PROPERTY(bool showChildNodeTypes READ showChildNodeTypes WRITE setShowChildNodeTypes)
@@ -156,7 +156,7 @@ public:
 
   /// Return the currently selected node id . "" if no node is selected
   /// Utility function that is based on currentNode
-  QString currentNodeId()const;
+  QString currentNodeID()const;
 
   /// if true, when the user create a node using "Add node", the node will be
   /// automatically selected. It doesn't apply if the node is programatically
@@ -239,13 +239,13 @@ public slots:
   void setCurrentNode(vtkMRMLNode* node);
 
   /// Select the node to be current. If \nodeId is invalid (or can't be found
-  /// in the scene), the current node becomes 0. 
-  void setCurrentNode(const QString& nodeID);
+  /// in the scene), the current node becomes 0.
+  void setCurrentNodeID(const QString& nodeID);
 
   /// Select the current node by index. The index refers to the order of the nodes
   /// into the list. If \a index is 0, the first node will be selected (even if
   /// "NoneEnabled" is true).
-  /// \sa nodeCount, setCurrentNode(vtkMRMLNode* ), setCurrentNode(const QString&)
+  /// \sa nodeCount, setCurrentNode(vtkMRMLNode* ), setCurrentNodeId(const QString&)
   void setCurrentNodeIndex(int index);
 
   /// Creates a node of the same type than on the "node types" properties.
@@ -268,7 +268,11 @@ signals:
   /// This signal is sent anytime the current node is changed. NULL if
   /// no node is current or the current item is "None".
   void currentNodeChanged(vtkMRMLNode* node);
-  
+
+  /// This signal is sent anytime the current node is changed. "" if
+  /// no node is current or the current item is "None".
+  void currentNodeIDChanged(const QString& id);
+
   /// Advanced function.
   /// This signal is sent when the user chooses a node in the combobox.
   /// The item's node is passed. Note that this signal is sent even when the 

Base/QTCLI/qSlicerCLIModuleUIHelper.cxx
@@ -94,16 +94,16 @@ WIDGET_VALUE_WRAPPER(FloatWithConstraints, ctkSliderWidget, value, setValue, Dou
 WIDGET_VALUE_WRAPPER(DoubleWithoutConstraints, ctkSpinBox, value, setValue, Double, valueChanged(double));
 WIDGET_VALUE_WRAPPER(DoubleWithConstraints, ctkSliderWidget, value, setValue, Double, valueChanged(double));
 WIDGET_VALUE_WRAPPER(String, QLineEdit, text, setText, String, textChanged(const QString&));
-WIDGET_VALUE_WRAPPER(Point, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
-WIDGET_VALUE_WRAPPER(Region, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
-WIDGET_VALUE_WRAPPER(Image, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
-WIDGET_VALUE_WRAPPER(Geometry, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
-WIDGET_VALUE_WRAPPER(Table, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
-WIDGET_VALUE_WRAPPER(Transform, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
+WIDGET_VALUE_WRAPPER(Point, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
+WIDGET_VALUE_WRAPPER(Region, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
+WIDGET_VALUE_WRAPPER(Image, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
+WIDGET_VALUE_WRAPPER(Geometry, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
+WIDGET_VALUE_WRAPPER(Table, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
+WIDGET_VALUE_WRAPPER(Transform, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
 WIDGET_VALUE_WRAPPER(Directory, ctkDirectoryButton, directory, setDirectory, String, directoryChanged(QString));
 WIDGET_VALUE_WRAPPER(File, ctkPathLineEdit, currentPath, setCurrentPath, String, currentPathChanged(QString));
 WIDGET_VALUE_WRAPPER(Enumeration, ButtonGroupWidgetWrapper, checkedValue, setCheckedValue, String, valueChanged());
-WIDGET_VALUE_WRAPPER(Measurement, qMRMLNodeComboBox, currentNodeId, setCurrentNode, String, currentNodeChanged(vtkMRMLNode*));
+WIDGET_VALUE_WRAPPER(Measurement, qMRMLNodeComboBox, currentNodeID, setCurrentNodeID, String, currentNodeIDChanged(QString));
 
 //-----------------------------------------------------------------------------
 #define INSTANCIATE_WIDGET_VALUE_WRAPPER(_NAME, _PARAM_NAME, _LABEL, _WIDGET_INSTANCE) \

Libs/MRML/Widgets/Testing/qMRMLSliceControllerWidgetTest.cxx
@@ -169,7 +169,7 @@ void qMRMLSliceControllerWidgetTester::testSetBackgroundVolume()
   QFETCH(QString, expectedVolumeNodeID);
   qMRMLNodeComboBox* comboBox =
     qobject_cast<qMRMLNodeComboBox*>(sliceControllerWidget.findChild<qMRMLNodeComboBox*>("BackgroundComboBox"));
-  QCOMPARE(comboBox->currentNodeId(), expectedVolumeNodeID);
+  QCOMPARE(comboBox->currentNodeID(), expectedVolumeNodeID);
 
   //sliceControllerWidget.show();
   //qApp->exec();
@@ -201,7 +201,7 @@ void qMRMLSliceControllerWidgetTester::testSetForegroundVolume()
   QFETCH(QString, expectedVolumeNodeID);
   qMRMLNodeComboBox* comboBox =
     qobject_cast<qMRMLNodeComboBox*>(sliceControllerWidget.findChild<qMRMLNodeComboBox*>("ForegroundComboBox"));
-  QCOMPARE(comboBox->currentNodeId(), expectedVolumeNodeID);
+  QCOMPARE(comboBox->currentNodeID(), expectedVolumeNodeID);
 
   //sliceControllerWidget.show();
   //qApp->exec();
@@ -233,7 +233,7 @@ void qMRMLSliceControllerWidgetTester::testSetLabelVolume()
   QFETCH(QString, expectedVolumeNodeID);
   qMRMLNodeComboBox* comboBox =
     qobject_cast<qMRMLNodeComboBox*>(sliceControllerWidget.findChild<qMRMLNodeComboBox*>("LabelMapComboBox"));
-  QCOMPARE(comboBox->currentNodeId(), expectedVolumeNodeID);
+  QCOMPARE(comboBox->currentNodeID(), expectedVolumeNodeID);
 
   //sliceControllerWidget.show();
   //qApp->exec();
@@ -267,7 +267,7 @@ void qMRMLSliceControllerWidgetTester::testChangeLabelMapToScalarVolume()
 
   qMRMLNodeComboBox* comboBox =
     qobject_cast<qMRMLNodeComboBox*>(sliceControllerWidget.findChild<qMRMLNodeComboBox*>("LabelMapComboBox"));
-  QCOMPARE(comboBox->currentNodeId(), QString());
+  QCOMPARE(comboBox->currentNodeID(), QString());
 }
 
 // ----------------------------------------------------------------------------
@@ -301,7 +301,7 @@ void qMRMLSliceControllerWidgetTester::testSetLabelVolumeWithNoLinkedControl()
 
   qMRMLNodeComboBox* comboBox =
     qobject_cast<qMRMLNodeComboBox*>(sliceControllerWidget.findChild<qMRMLNodeComboBox*>("LabelMapComboBox"));
-  QCOMPARE(comboBox->currentNodeId(), QString(labelmapNode2->GetID()));
+  QCOMPARE(comboBox->currentNodeID(), QString(labelmapNode2->GetID()));
 
   //sliceControllerWidget.show();
   //qApp->exec();

Libs/MRML/Widgets/qMRMLChartViewControllerWidget.cxx
@@ -346,7 +346,7 @@ void qMRMLChartViewControllerWidget::updateWidgetFromMRML()
     }
 
   // ChartNode selector
-  d->chartComboBox->setCurrentNode(chartNode->GetID());
+  d->chartComboBox->setCurrentNodeID(chartNode->GetID());
 
   // Array selector
   vtkStringArray *arrayIDs = chartNode->GetArrays();

Libs/MRML/Widgets/qMRMLNodeComboBoxEventPlayer.cpp
@@ -61,7 +61,7 @@ bool qMRMLNodeComboBoxEventPlayer::playEvent(QObject *Object,
         {
         comboBox->setCurrentNodeIndex(0);
         }
-      comboBox->setCurrentNode(Arguments);
+      comboBox->setCurrentNodeID(Arguments);
       return true;
       }
     if (Command == "nodeAboutToBeRemoved")

Modules/Loadable/Models/Widgets/qMRMLModelDisplayNodeWidget.cxx
@@ -215,5 +215,5 @@ void qMRMLModelDisplayNodeWidget::updateWidgetFromMRML()
   d->ActiveScalarComboBox->blockSignals(wasBlocking);
   d->ActiveScalarComboBox->setCurrentArray(d->MRMLModelDisplayNode->GetActiveScalarName());
   d->ScalarsColorTableComboBox->setMRMLScene(d->MRMLModelDisplayNode->GetScene());
-  d->ScalarsColorTableComboBox->setCurrentNode(d->MRMLModelDisplayNode->GetColorNodeID());
+  d->ScalarsColorTableComboBox->setCurrentNodeID(d->MRMLModelDisplayNode->GetColorNodeID());
 }

Modules/Loadable/TractographyDisplay/Widgets/qSlicerTractographyDisplayWidget.cxx
@@ -623,7 +623,7 @@ void qSlicerTractographyDisplayWidget::updateWidgetFromMRML()
   d->VisibilityCheckBox->setChecked( d->FiberBundleDisplayNode->GetVisibility() );
   d->OpacitySlider->setValue( d->FiberBundleDisplayNode->GetOpacity() );
   
-  d->ColorByScalarsColorTableComboBox->setCurrentNode
+  d->ColorByScalarsColorTableComboBox->setCurrentNodeID
     (d->FiberBundleDisplayNode->GetColorNodeID());
   d->ColorByScalarComboBox->setDataSet(vtkDataSet::SafeDownCast(d->FiberBundleNode->GetPolyData()));
 

commit 0a23a5dc219f4c3c23dac0a96a6474eee3a5185e

BUG: Ensure backward compatibility in qMRMLNodeComboBox
Here is a quick summary of what happened:

What changed in r22052:
 Rename the Q_Property from currentNodeId to currentNodeID
 Rename QString currentNodeId() to QString currentNodeID()
 Rename setCurrentNode(const QString nodeID)
  to setCurrentNodeID(const QString nodeID)

What changed in r22059:
 Add QString currentNodeId() as a deprecated function
  (not invokable though)

What is broken after r22052 and r22059 ?:
 In C++, the function setCurrentNode(const QString nodeID) is missing.
 In Python:
  The function currentNodeId() is missing (not wrapped due to a missing
   Q_INVOKABLE).
  Trickier, any code using the property currentNodeId = ... will break too.

What this commit intend to fix:
  Add Q_INVOKABLE to currentNodeId() method for python wrapping.
  Return the old property as deprecated.

Also adding warning in the deprecated function to warn other developers.

See Issue #1694

git-svn-id: http://svn.slicer.org/Slicer4/trunk@22072 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLNodeComboBox.cxx
@@ -20,6 +20,7 @@
 
 // Qt includes
 #include <QApplication>
+#include <QDebug>
 #include <QHBoxLayout>
 #include <QInputDialog>
 #include <QKeyEvent>
@@ -468,7 +469,7 @@ QString qMRMLNodeComboBox::currentNodeID()const
 // --------------------------------------------------------------------------
 QString qMRMLNodeComboBox::currentNodeId()const
 {
-  // \deprecated
+  qWarning() << "This function is deprecated. Use currentNodeID() instead";
   return this->currentNodeID();
 }
 
@@ -618,6 +619,13 @@ void qMRMLNodeComboBox::setMRMLScene(vtkMRMLScene* scene)
 void qMRMLNodeComboBox::setCurrentNode(vtkMRMLNode* newCurrentNode)
 {
   this->setCurrentNodeID(newCurrentNode ? newCurrentNode->GetID() : "");
+}
+
+// --------------------------------------------------------------------------
+void qMRMLNodeComboBox::setCurrentNode(const QString& nodeID)
+{
+  qWarning() << "This function is deprecated. Use setCurrentNodeID() instead";
+  this->setCurrentNodeID(nodeID);
 }
 
 // --------------------------------------------------------------------------

Libs/MRML/Widgets/qMRMLNodeComboBox.h
@@ -57,6 +57,7 @@ class QMRML_WIDGETS_EXPORT qMRMLNodeComboBox
 {
   Q_OBJECT
   Q_PROPERTY(QString currentNodeID READ currentNodeID WRITE setCurrentNodeID NOTIFY currentNodeIDChanged DESIGNABLE false)
+  Q_PROPERTY(QString currentNodeId READ currentNodeId WRITE setCurrentNode DESIGNABLE false) // \deprecated
   Q_PROPERTY(QStringList nodeTypes READ nodeTypes WRITE setNodeTypes)
   Q_PROPERTY(bool showHidden READ showHidden WRITE setShowHidden)
   Q_PROPERTY(bool showChildNodeTypes READ showChildNodeTypes WRITE setShowChildNodeTypes)
@@ -160,7 +161,7 @@ public:
 
   /// \deprecated
   /// Use currentNodeID instead
-  QString currentNodeId()const;
+  Q_INVOKABLE QString currentNodeId()const;
 
   /// if true, when the user create a node using "Add node", the node will be
   /// automatically selected. It doesn't apply if the node is programatically
@@ -242,6 +243,10 @@ public slots:
   /// Select the node to be current
   void setCurrentNode(vtkMRMLNode* node);
 
+  /// \deprecated
+  /// Use setCurrentNodeID instead
+  void setCurrentNode(const QString& nodeID);
+
   /// Select the node to be current. If \nodeId is invalid (or can't be found
   /// in the scene), the current node becomes 0.
   void setCurrentNodeID(const QString& nodeID);
@@ -249,7 +254,7 @@ public slots:
   /// Select the current node by index. The index refers to the order of the nodes
   /// into the list. If \a index is 0, the first node will be selected (even if
   /// "NoneEnabled" is true).
-  /// \sa nodeCount, setCurrentNode(vtkMRMLNode* ), setCurrentNodeId(const QString&)
+  /// \sa nodeCount, setCurrentNode(vtkMRMLNode* ), setCurrentNodeID(const QString&)
   void setCurrentNodeIndex(int index);
 
   /// Creates a node of the same type than on the "node types" properties.